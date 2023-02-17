2bda7c1b13c63831b14d4f0acc482af53c6150f8
Eneyee <122353277+Eneyee@users.noreply.github.com>
2023-02-17 13:36:16 +0200

    updated scripts

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	inputs.forEach((input) => {
+		
+		if (!input.value) {
+			input.parentElement.classList.add('error');
+		} else {
+			input.parentElement.classList.remove('error');
+			if (input.type == 'email') {
+				if (validateEmail(input.value)) {
+					input.parentElement.classList.remove('error');
+				} else {
+					input.parentElement.classList.add('error');
+				}
+			}
+		}
+	});
+});
