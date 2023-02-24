fd06945be9308f193b36791221a6c72f1493ab00
Eneyee <max.aygelson@gmail.com>
2023-02-24 12:23:12 +0200

    reworked <main>, <footer>, and "login-container"

--- scripts.js
+++ scripts.js
@@ -7,13 +7,17 @@ form.addEventListener('submit', (e) => {
 		
 		if (!input.value) {
 			input.parentElement.classList.add('error');
+			input.parentElement.classList.add('toggle');
 		} else {
 			input.parentElement.classList.remove('error');
+			input.parentElement.classList.remove('toggle');
 			if (input.type == 'email') {
 				if (validateEmail(input.value)) {
 					input.parentElement.classList.remove('error');
+					input.parentElement.classList.remove('toggle');
 				} else {
 					input.parentElement.classList.add('error');
+					input.parentElement.classList.add('toggle');
 				}
 			}
 		}
